4b2768fbf62702eee826bd42192146ba7b71d4a6
duc.le1 <duc.le1@bytedance.com>
2023-05-18 05:39:09 +0800

    feat: use indexed webdataset for mcc

--- recipes/musiclm/datasets/mcc.py
+++
@@ -1,4 +0,0 @@
-def get_mcc_shardlist(fname):
-    with open(fname, "r") as fp:
-        urls = [f"pipe:hdfs dfs -cat {line}" for line in fp.readlines()]
-    return urls
--- recipes/musiclm/transforms/musiclm.py
+++ recipes/musiclm/transforms/musiclm.py
@@ -1,4 +1,4 @@
-from typing import Dict, Generator, Optional
+from typing import Any, Dict, Generator, Optional
 import io
 import torch
 from torchaudio_augmentations import Compose
@@ -72,10 +72,10 @@ class MCCTransforms(TransformBase):
         self,
         n_samples: int,
         sample_rate: int,
-        metalist_path: str,
         audio_key: str = "mp3",
         min_volume_threshold: float = 0.05,
         loudness_ratio_threshold: float = 0.5,
+        aed_filtered: bool = True,
     ) -> None:
         super().__init__()
         self.n_samples = n_samples
@@ -83,9 +83,7 @@ class MCCTransforms(TransformBase):
         self.audio_key = audio_key
         self.min_volume_threshold = min_volume_threshold
         self.loudness_ratio_threshold = loudness_ratio_threshold
-
-        with open(metalist_path, "r") as fp:
-            self.metalist = [self.metalist2id(line.strip()) for line in fp.readlines()]
+        self.aed_filtered = aed_filtered
 
         self.is_loud = LoudnessCheck(
             self.sample_rate,
@@ -109,39 +107,19 @@ class MCCTransforms(TransformBase):
         self.random_pad = RandomPad(n_samples=n_samples)
         self.random_crop = RandomResizedCrop(n_samples=n_samples)
 
-    def key2id(self, url, key):
-        """
-        url: 'pipe:hdfs dfs -cat hdfs://harunava/home/byte_speech_sv/data/genre_balanced_mcc_clean/classical.2.57/0.tar'
-        key: '6980478627137718274'
-        return: 'classical.2.57/0/6980478627137718274'
-        """
-        sub_folder = url.split("/")[-2]
-        tar_name = url.split("/")[-1].split(".")[0]
-        return f"{sub_folder}/{tar_name}/{key}"
+    def __call__(self, x: Dict[str, Any]) -> Generator:
+        if self.aed_filtered and not x["__index_data__"].get("aed_filtered", False):
+            self._update_stats(skipped=True)
+            return
 
-    def metalist2id(self, path):
-        """
-        path: '../genre_balanced_mcc_clean_tags/8-bit.0.54/0/0-7196752726452242434.json'
-        return: '8-bit.0.54/0/7196752726452242434'
-        """
-        sub_folder = path.split("/")[-3]
-        tar_name = path.split("/")[-2]
-        id = path.split("/")[-1].split(".")[0].split("-")[-1]
-        return f"{sub_folder}/{tar_name}/{id}"
-
-    def __call__(self, x: Dict[str, torch.Tensor]) -> Generator:
         audio = self.base_transform(io.BytesIO(x[self.audio_key]))
-
         audio = self.random_pad(audio)
         # Return up to self.num_crops crops
         max_num_crops = audio.size(1) // self.n_samples
         num_crops = 0
-
-        if self.key2id(x["__url__"], x["__key__"]) in self.metalist:
-            while num_crops < max_num_crops:
-                cropped_audio = self.random_crop(audio)
-                if self.is_loud(cropped_audio):
-                    yield {"audio.npy": cropped_audio}
-                num_crops += 1
-        else:
-            self._update_stats(skipped=True)
+        while num_crops < max_num_crops:
+            cropped_audio = self.random_crop(audio)
+            if self.is_loud(cropped_audio):
+                yield {"audio.npy": cropped_audio}
+            num_crops += 1
+        self._update_stats(skipped=False)
