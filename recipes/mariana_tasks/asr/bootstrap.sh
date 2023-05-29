f2fd4c681483c479fe7fb783b75beead67aa5eac
wangxin.colin <wangxin.colin@bytedance.com>
2023-05-29 17:52:21 +0800

    feat(image): add mariana related dependencies

--- recipes/mariana_tasks/asr/bootstrap.sh
+++ recipes/mariana_tasks/asr/bootstrap.sh
@@ -1,13 +1,8 @@
+#!/bin/bash -ex
+
 cd $(dirname $0)/../../../
 
 WORK_DIR=$(pwd)
-echo "work dir: $WORK_DIR"
-
-export SAMANTHA_DISABLE_LOGGING_BASIC_CONFIG=1
-export NCCL_DEBUG=WARN
-export PYTHONPATH=/opt/tiger/Megatron-LM/:$WORK_DIR/mariana:$PYTHONPATH
-export TOKENIZERS_PARALLELISM=false
-
 
 # for debug
 # --trainer.logger=['console'] \
@@ -16,8 +11,11 @@ export TOKENIZERS_PARALLELISM=false
 # --model.network.n_inner=1024 \
 # --model.partial_pretrain=/mnt/bn/by-nas/Speech/Codes/LanguageModels/ckpts/Seed-13B-SFT-P2.0.0_D4.0.0_T600B-SFT3.8.0/checkpoints/global_step_2860/zero3_merge_states_with_bestrq_codes.pt \
 
+hdfs dfs -get hdfs://haruna/home/byte_speech_sv/user/wangxin.colin/experiments/mariana/training_miscs
+hdfs dfs -get hdfs://haruna/home/byte_speech_sv/user/wangxin.colin/experiments/mariana/bbpe64k-0303
+hdfs dfs -get hdfs://haruna/home/byte_speech_sv/user/wangxin.colin/experiments/mariana/global_step_2860_zero3_merge_states_with_bestrq_codes.pt
 
-TORCHRUN -m recipes.mariana_tasks.asr.train \
+bash recipes/mariana_tasks/train.sh -m recipes.mariana_tasks.asr.train \
 --model=recipes/mariana_tasks/configs/13b_v120_bbpe_64k.yaml \
 --model.network.vocab_size=65026 \
 --model.network.use_rmpad=true \
@@ -29,7 +27,7 @@ TORCHRUN -m recipes.mariana_tasks.asr.train \
 --model.network.resid_pdrop=0.1 \
 --model.network.embd_pdrop=0.0 \
 --model.network.attn_pdrop=0.1 \
---model.partial_pretrain=/mnt/bn/by-nas/Speech/Codes/LanguageModels/ckpts/Seed-13B-SFT-P2.0.0_D4.0.0_T600B-SFT3.8.0/checkpoints/global_step_2860/zero3_merge_states_with_bestrq_codes.pt \
+--model.partial_pretrain=${WORK_DIR}/global_step_2860_zero3_merge_states_with_bestrq_codes.pt \
 --trainer=recipes/mariana_tasks/configs/zero3-no-cg.yaml \
 --trainer.max_epochs=20 \
 --data.train_size=113308140 \
@@ -39,12 +37,12 @@ TORCHRUN -m recipes.mariana_tasks.asr.train \
 --trainer.optimizer_kwargs.optimizer.params.lr=1e-5 \
 --trainer.accumulate_grad_batches=4 \
 --trainer.gradient_clip_val=1.0 \
---trainer.default_hdfs_dir=hdfs://haruna/home/byte_speech_sv/user/wangxin.colin/experiments/mariana/test_spokenllm \
+--trainer.default_hdfs_dir=hdfs://haruna/home/byte_speech_sv/user/wangxin.colin/experiments/mariana/test_spokenllm_new_icm \
 --data.train_path=hdfs://haruna/home/byte_speech_sv/user/wangxin.colin/test_by_asr/train_sub*_rank*_shard*.parquet \
 --data.val_path=hdfs://haruna/home/byte_speech_sv/user/wangxin.colin/test_by_asr/cv/rank*_shard*.parquet \
 --data.source_types=['parquet'] \
 --data.tokenizer_type=bbpe \
---data.tokenizer=/mnt/bn/by-nas/Speech/Codes/LanguageModels/bbpe64k-0303 \
---data.template_fn=/mnt/bn/by-nas/Speech/Codes/LanguageModels/training_miscs/templates_v0.1.txt \
---data.val_template_fn=/mnt/bn/by-nas/Speech/Codes/LanguageModels/training_miscs/templates.txt \
+--data.tokenizer=${WORK_DIR}/bbpe64k-0303 \
+--data.template_fn=${WORK_DIR}/training_miscs/templates_v0.1.txt \
+--data.val_template_fn=${WORK_DIR}/training_miscs/templates.txt \
 --merge_zero3_states=True "$@"
---
+++ recipes/mariana_tasks/train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+cd $(dirname $0)/../../
+
+WORK_DIR=$(pwd)
+echo "work dir: $WORK_DIR"
+
+export SAMANTHA_DISABLE_LOGGING_BASIC_CONFIG=1
+export NCCL_DEBUG=WARN
+
+export TOKENIZERS_PARALLELISM=false
+export PYTHONPATH=/opt/tiger/Megatron-LM/:$WORK_DIR/mariana:$PYTHONPATH
+
+TORCHRUN "$@"
