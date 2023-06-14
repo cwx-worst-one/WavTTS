d12af53f2c3386d1e14cbb9f1707aae58fa5734b
Jingsong Gao <jingsonggao@bytedance.com>
2023-06-13 17:11:11 -0700

    feat: sync mulan from sami_ai_models

--- recipes/mulan/bootstrap.sh
+++ recipes/mulan/bootstrap.sh
@@ -9,6 +9,9 @@ pip3 install -U --pre triton -i https://bytedpypi.byted.org/simple
 
 export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512'
 
+# Download mae ckpt
+hdfs dfs -get "/home/byte_speech_sv/weitsung.lu/mut_mae/MuT_MAE/mut_large/mutmae-step=177600-loss_1=5-sf.pth"
+
 # Download gpt3 expansion
 hdfs_base="hdfs://harunava/home/byte_speech_sv/mulan"
 gpt3_expansion="assets/ecals_gpt3_expansion.pkl"
@@ -19,9 +22,10 @@ if [ ! -d "assets/" ]
 then
     echo "Download ecals gpt3 expansion"
     mkdir -p assets
-    hdfs dfs -get $hdfs_base/$gpt3_expansion $gpt3_expansion
-    hdfs dfs -get $hdfs_base/$g4_aed $g4_aed
-    hdfs dfs -get $hdfs_base/$g4_mcc $g4_mcc
+    # hdfs dfs -get $hdfs_base/$gpt3_expansion $gpt3_expansion
+    # hdfs dfs -get $hdfs_base/$g4_aed $g4_aed
+    # hdfs dfs -get $hdfs_base/$g4_mcc $g4_mcc
+    hdfs dfs -get $hdfs_base/assets
 else
     echo "Ecals gpt3 expansion exists, skip download"
 fi
--- recipes/mulan/dataset/mcc.py
+++ recipes/mulan/dataset/mcc.py
@@ -24,12 +24,25 @@ MCC_N2M_URLS = [
 ]
 
 
+def filter_g4_category_labels(s):
+    if ":" not in s:
+        return s
+    x = s.index(":")
+    prefix = s[:x].lower()
+    for k in ["genre", "mood", "theme", "epoch", "musician", "level", "experience"]:
+        if k in prefix:
+            return s[x + 1 :].strip()
+    return s
+
+
 class MCCGPTGenDataset(IterableDataset):
     def __init__(self, mode="train", **kwargs):
         self.tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased")
         self.dataset = (
             wds.WebDataset(MCC_GPT_GEN_URLS, **kwargs)
             .decode()
+            .to_tuple("__key__", "meta.json", "audio.npy")
+            .map(lambda x: {"chunk_id": x[0], "meta.json": x[1], "audio.npy": x[2]})
             .map(utils.process_audio)
             .map(self._process_text)
             .map(utils.tokenize_text(self.tokenizer, mode))
@@ -47,36 +60,67 @@ class MCCGPTGenDataset(IterableDataset):
 
 
 class AEDGPTGenDataset(IterableDataset):
-    def __init__(self, mode="train", **kwargs):
+    def __init__(self, mode="train", verbose=False, **kwargs):
+        self.verbose = verbose
         self.tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased")
         self.dataset = (
             wds.WebDataset(AED_GPT_GEN_URLS, **kwargs)
             .decode()
+            .to_tuple("__key__", "meta.json", "audio.npy")
+            .map(lambda x: {"chunk_id": x[0], "meta.json": x[1], "audio.npy": x[2]})
             .map(utils.process_audio)
             .map(self._process_text)
             .map(utils.tokenize_text(self.tokenizer, mode))
         )
         with open("assets/chatgpt_g4_aed.pkl", "rb") as f:
             self.chatgpt_g4 = pickle.load(f)
+        with open("assets/n2m_aed.pkl", "rb") as f:
+            self.n2m = pickle.load(f)
 
     def _process_text(self, data):
         meta = data["meta.json"]
 
         text = ""
         if random.random() < 0.5:
-            text = meta["gpt_generated_caption"]
+            if data["chunk_id"] in self.n2m:
+                tags = list(self.n2m[data["chunk_id"]])
+                random.shuffle(tags)
+                for tag in tags:
+                    if random.random() < 0.8:
+                        text += tag + " "
+                text = text.strip()
+                if self.verbose:
+                    chunk_id = data["chunk_id"]
+                    print(
+                        f"AED N2M sample - chunk_id = {chunk_id}, n2m candidates is {tags}, final text is {text}"
+                    )
+            else:
+                if self.verbose:
+                    print(f"chunk_id not found in this AED example!")
+
+            if text == "":
+                text = meta["gpt_generated_caption"]
         else:
             if str(meta["music_id"]) in self.chatgpt_g4:
                 tags = self.chatgpt_g4[str(meta["music_id"])]
                 # get all the items into list
-                tags = [v for v in tags.values()]
+                tags = [
+                    filter_g4_category_labels(tags[k])
+                    for k in tags
+                    if "instrument" not in k.lower() and "place" not in k.lower()
+                ]
+                tags = [s for s in tags if len(s) > 0]
                 # randomize the order
                 random.shuffle(tags)
                 for tag in tags:
                     if random.random() < 0.8:
-                        text += tag
-        data["text"] = text
-
+                        text += tag + " "
+                if self.verbose:
+                    chunk_id = data["chunk_id"]
+                    print(
+                        f"AED G4 sample - chunk_id = {chunk_id}, G4 candidates is {tags}, final text is {text}"
+                    )
+        data["text"] = text.strip()
         data["data_source"] = meta["data_source"]
         data["music_id"] = meta["music_id"]
         return data
@@ -86,17 +130,22 @@ class AEDGPTGenDataset(IterableDataset):
 
 
 class MCCN2MDataset(IterableDataset):
-    def __init__(self, mode="train", **kwargs):
+    def __init__(self, mode="train", verbose=False, **kwargs):
+        self.verbose = verbose
         self.tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased")
         self.dataset = (
             wds.WebDataset(MCC_N2M_URLS, **kwargs)
             .decode()
+            .to_tuple("__key__", "meta.json", "audio.npy")
+            .map(lambda x: {"chunk_id": x[0], "meta.json": x[1], "audio.npy": x[2]})
             .map(utils.process_audio)
             .map(self._process_text)
             .map(utils.tokenize_text(self.tokenizer, mode))
         )
         with open("assets/chatgpt_g4_mcc.pkl", "rb") as f:
             self.chatgpt_g4 = pickle.load(f)
+        with open("assets/n2m_mcc.pkl", "rb") as f:
+            self.n2m = pickle.load(f)
 
     def _process_text(self, data):
         meta = data["meta.json"]
@@ -104,19 +153,39 @@ class MCCN2MDataset(IterableDataset):
         # data["text"] = random.choice(n2m_texts)
         text = ""
         if random.random() < 0.5:
-            n2m_texts = meta["n2m"]
-            text += random.choice(n2m_texts)
+            if data["chunk_id"] in self.n2m:
+                tags = list(self.n2m[data["chunk_id"]])
+                random.shuffle(tags)
+                for tag in tags:
+                    if random.random() < 0.8:
+                        text += tag + " "
+                text = text.strip()
+                if self.verbose:
+                    chunk_id = data["chunk_id"]
+                    print(
+                        f"MCC N2M sample - chunk_id = {chunk_id}, N2M candidates is {tags}, final text is {text}"
+                    )
         else:
             if str(meta["music_id"]) in self.chatgpt_g4:
                 tags = self.chatgpt_g4[str(meta["music_id"])]
                 # get all the items into list
-                tags = [v for v in tags.values()]
+                tags = [
+                    filter_g4_category_labels(tags[k])
+                    for k in tags
+                    if "instrument" not in k.lower() and "place" not in k.lower()
+                ]
+                tags = [s for s in tags if len(s) > 0]
                 # randomize the order
                 random.shuffle(tags)
                 for tag in tags:
                     if random.random() < 0.8:
-                        text += tag
-        data["text"] = text
+                        text += tag + " "
+                if self.verbose:
+                    chunk_id = data["chunk_id"]
+                    print(
+                        f"MCC G4 sample - chunk_id = {chunk_id}, G4 candidates is {tags}, final text is {text}"
+                    )
+        data["text"] = text.strip()
         data["data_source"] = meta["data_source"]
         data["music_id"] = meta["music_id"]
 
@@ -124,3 +193,19 @@ class MCCN2MDataset(IterableDataset):
 
     def __iter__(self):
         return iter(self.dataset)
+
+
+if __name__ == "__main__":
+
+    g4_dataset = MCCGPTGenDataset()
+    g4_loader = wds.WebLoader(g4_dataset, num_workers=1, batch_size=None)
+
+    for batch in g4_loader:
+        print(batch)
+        break
+
+    n2m_dataset = MCCN2MDataset()
+    n2m_loader = wds.WebLoader(n2m_dataset, num_workers=1, batch_size=None)
+    for batch in n2m_loader:
+        print(batch)
+        break
