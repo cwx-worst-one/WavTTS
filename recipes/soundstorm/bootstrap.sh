075a514bf3f61db43757547d6abae881b28460cc
Janne Spijkervet <janne.spijkervet@bytedance.com>
2023-06-17 08:37:17 +0000

    fix: batch size for mcc40m soundstorm training
    * fix: batch size for mcc40m soundstorm training
    
    https://code.byted.org/seed/samantha/merge_requests/101

--- recipes/soundstorm/bootstrap.sh
+++ recipes/soundstorm/bootstrap.sh
@@ -4,4 +4,10 @@ cd $(dirname $0)/../../
 
 pip3 install -qr ./recipes/soundstorm/requirements.txt
 
+if [[ -z "${ARNOLD_TRIAL_ID}" ]]; then
+    export ARNOLD_OUTPUT="logs"
+else
+    export ARNOLD_OUTPUT=/mnt/bn/audio-diffusion/logs/soundstorm/${ARNOLD_TRIAL_ID}
+fi
+
 sh launch.sh $@
