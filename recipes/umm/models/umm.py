UMM - Chroma enhanced & ROPE precision fix
* fix: minor
* fix: minor
* fix: dataloader
* fix: minor
* fix: minor
* fix: encoding
* fix: added bootstrap
* fix: minor
* fix: add length check
* feat: coarse
* feat: coarse
* fix: minor
* fix: rope fp32
* fix: clean files
* fix: lint
* fix: update coarse config

https://code.byted.org/seed/samantha/merge_requests/231

recipes/umm/models/umm.py
@@ -11,6 +11,7 @@ from transformers.utils import ModelOutput
 
 from recipes.umm.models.vocoder import BigVGAN
 from recipes.umm.models.vq import EMAVectorQuantizer, VectorQuantize
+from recipes.umm.transforms.chroma import ChromaSpectrogram
 from recipes.umm.transforms.speech import SpeechTransform
 
 
@@ -31,29 +32,31 @@ class ConformerRotaryPositionalEmbedding(nn.Module):
 
         inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float() / dim))
         self.register_buffer("inv_freq", inv_freq)
-        self.cached_sequence_length = None
+        self.cached_sequence_length = 0
         self.cached_rotary_positional_embedding = None
 
-    def forward(self, hidden_states):
-        sequence_length = hidden_states.shape[1]
-
-        if (
-            sequence_length == self.cached_sequence_length
-            and self.cached_rotary_positional_embedding is not None
-        ):
-            return self.cached_rotary_positional_embedding
-
+    @torch.cuda.amp.autocast(enabled=False)
+    def _set_cos_sin_cache(self, sequence_length):
         self.cached_sequence_length = sequence_length
-        time_stamps = torch.arange(sequence_length).type_as(self.inv_freq)
+        time_stamps = torch.arange(
+            sequence_length, device=self.inv_freq.device, dtype=torch.float32
+        )
         freqs = torch.einsum("i,j->ij", time_stamps, self.inv_freq)
         embeddings = torch.cat((freqs, freqs), dim=-1)
-
         cos_embeddings = embeddings.cos()[:, None, None, :]
         sin_embeddings = embeddings.sin()[:, None, None, :]
         self.cached_rotary_positional_embedding = torch.stack(
             [cos_embeddings, sin_embeddings]
         )
-        return self.cached_rotary_positional_embedding
+
+    def forward(self, hidden_states):
+        sequence_length = hidden_states.shape[1]
+        if (
+            sequence_length > self.cached_sequence_length
+            or self.cached_rotary_positional_embedding is None
+        ):
+            self._set_cos_sin_cache(sequence_length)
+        return self.cached_rotary_positional_embedding[:, -sequence_length:]
 
 
 class ConformerFeedForward(nn.Module):
@@ -772,7 +775,17 @@ class FineTunedModel(BaseModel):
         if config.add_vocoder:
             self.vocoder = BigVGAN(config)
         if config.get("add_chroma", False):
-            self.chromarecon_head = Conv2dUpsampling(self.config.hidden_size, 12)
+            self.chroma_transform = ChromaSpectrogram(
+                sample_rate=config.sample_rate,
+                n_fft=config.n_fft,
+                win_length=config.win_length,
+                hop_length=config.hop_length,
+                n_chroma=config.n_chroma,
+                normalized=False,
+            )
+            self.chromarecon_head = Conv2dUpsampling(
+                self.config.hidden_size, config.n_chroma
+            )
         del self.unfolder
         del self.rq
         del self.rq_head
@@ -814,6 +827,18 @@ class FineTunedModel(BaseModel):
                 vq_embeds=vq_embeds if self.config.add_mulan else None,
             )
         return output_dict
+
+    @torch.no_grad()
+    @torch.cuda.amp.autocast(enabled=False)
+    def preprocessing(self, x):
+        normalize = self.config.feature_cmvn is not None
+        mel = self.audio_transform(x, normalize=normalize)
+        input_dict = {"feature": mel}
+        if self.config.get("add_chroma", False):
+            chroma = self.chroma_transform(x)[:, :, :-1].transpose(1, 2)
+            chroma = F.normalize(chroma, p=2, dim=-1)
+            input_dict.update(chroma=chroma)
+        return input_dict
 
 
 class FineTunedVocoder(BaseModel):

recipes/umm/models/umm_mkii.py
@@ -31,29 +31,31 @@ class ConformerRotaryPositionalEmbedding(nn.Module):
 
         inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float() / dim))
         self.register_buffer("inv_freq", inv_freq)
-        self.cached_sequence_length = None
+        self.cached_sequence_length = 0
         self.cached_rotary_positional_embedding = None
 
-    def forward(self, hidden_states):
-        sequence_length = hidden_states.shape[1]
-
-        if (
-            sequence_length == self.cached_sequence_length
-            and self.cached_rotary_positional_embedding is not None
-        ):
-            return self.cached_rotary_positional_embedding
-
+    @torch.cuda.amp.autocast(enabled=False)
+    def _set_cos_sin_cache(self, sequence_length):
         self.cached_sequence_length = sequence_length
-        time_stamps = torch.arange(sequence_length).type_as(self.inv_freq)
+        time_stamps = torch.arange(
+            sequence_length, device=self.inv_freq.device, dtype=torch.float32
+        )
         freqs = torch.einsum("i,j->ij", time_stamps, self.inv_freq)
         embeddings = torch.cat((freqs, freqs), dim=-1)
-
         cos_embeddings = embeddings.cos()[:, None, None, :]
         sin_embeddings = embeddings.sin()[:, None, None, :]
         self.cached_rotary_positional_embedding = torch.stack(
             [cos_embeddings, sin_embeddings]
         )
-        return self.cached_rotary_positional_embedding
+
+    def forward(self, hidden_states):
+        sequence_length = hidden_states.shape[1]
+        if (
+            sequence_length > self.cached_sequence_length
+            or self.cached_rotary_positional_embedding is None
+        ):
+            self._set_cos_sin_cache(sequence_length)
+        return self.cached_rotary_positional_embedding[:, -sequence_length:]
 
 
 class ConformerFeedForward(nn.Module):

recipes/umm/modules/criterion.py
@@ -171,13 +171,26 @@ class Stage2Loss(nn.Module):
         super().__init__()
         self.ctc_loss_fn = nn.CTCLoss()
         self.stft_loss_fn = STFTLoss()
+        self.chroma_loss_fn = STFTLoss()
 
-    def forward(self, recon_feature, feature, logits, text_ids):
+    def forward(
+        self, recon_feature, feature, logits, text_ids, recon_chroma=None, chroma=None
+    ):
         recon_feature = recon_feature.contiguous().float()
         feature = feature.contiguous().float()
 
         # Spec
         loss_dict = self.stft_loss_fn.float()(recon_feature, feature)
+
+        if recon_chroma is not None and chroma is not None:
+            recon_chroma = recon_chroma.contiguous().float()
+            chroma = chroma.contiguous().float()
+            chroma_loss = self.chroma_loss_fn.float()(recon_chroma, chroma)
+            loss_dict.update(
+                chroma_stft_loss=chroma_loss["stft_loss"],
+                chroma_spec_mag_loss=loss_dict["spec_mag_loss"],
+                chroma_lin_mag_loss=chroma_loss["lin_mag_loss"],
+            )
 
         # CTC
         input_lengths = torch.full((logits.size(0),), logits.size(1), dtype=torch.long)

recipes/umm/modules/lit_module.py
@@ -3,9 +3,12 @@ from typing import List, Optional, Union
 import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
+from einops import rearrange
 from pytorch_lightning.profilers import PassThroughProfiler
+from tqdm import tqdm
 from transformers import BertTokenizer
 
+from recipes.musiclm.inference.utils import sample
 from recipes.umm.models.utils import clip_grad_value_, mel_spectrogram_torch
 from recipes.umm.modules.criterion_vocoder import (
     MultiResolutionSTFTLoss,
@@ -943,26 +946,8 @@ class Stage2(BaseStage):
         missing_keys, unexpected_keys = self.load_state_dict(
             state_dict=state_dict, strict=False
         )
-        assert all(
-            any(
-                name.startswith(prefix_key)
-                for prefix_key in ["model.rq_head", "model.rq"]
-            )
-            for name in unexpected_keys
-        )
-        assert all(
-            any(
-                name.startswith(prefix_key)
-                for prefix_key in [
-                    "model.vq",
-                    "model.mulan_head",
-                    "model.ctc_head",
-                    "model.melrecon_head",
-                    "model.vocoder",
-                ]
-            )
-            for name in missing_keys
-        )
+        print(f"[Missing] {missing_keys}")
+        print(f"[Unexpected] {unexpected_keys}")
         if self.model.config.add_mulan:
             print(f'Loading mulan from {mulan["ckpt_path"]}')
             mulan = self.hparams.required_modules["mulan"]
@@ -976,7 +961,7 @@ class Stage2(BaseStage):
     def prepare_feature(self, batch):
         audio = batch["audio"].squeeze(dim=1).float()
         audio = self.pad_audio(audio)
-        feature = self.preprocessing(audio)
+        input_dict = self.preprocessing(audio)
         encoded_text = self.tokenizer(
             batch["text"],
             add_special_tokens=False,
@@ -984,7 +969,7 @@ class Stage2(BaseStage):
             return_tensors="pt",
         )
         text_ids = encoded_text["input_ids"].to(audio.device)
-        input_dict = {"feature": feature, "text_ids": text_ids, "wav": audio}
+        input_dict.update({"text_ids": text_ids, "wav": audio})
         if self.model.config.add_mulan:
             mulan_embeds = self.get_mulan_embeds(audio, data_type="music")
             input_dict["mulan_embeds"] = mulan_embeds
@@ -998,6 +983,9 @@ class Stage2(BaseStage):
             text_ids = input_dict["text_ids"]
             logits = output_dict["logits"]
             recon_feature = output_dict["recon_feature"]
+            if self.model.config.get("add_chroma", False):
+                chroma = input_dict["chroma"]
+                recon_chroma = output_dict["recon_chroma"]
             # if self.model.config.add_mulan:
             #     mulan_embeds = input_dict["mulan_embeds"]
             #     vq_embeds = output_dict["vq_embeds"]
@@ -1009,11 +997,21 @@ class Stage2(BaseStage):
                 feature=feature,
                 logits=logits,
                 text_ids=text_ids,
+                recon_chroma=recon_chroma
+                if self.model.config.get("add_chroma", False)
+                else None,
+                chroma=chroma if self.model.config.get("add_chroma", False) else None,
             )
             loss_dict["loss"] = (
                 loss_dict["ctc_loss"] * self.model.config.w_ctc_loss
                 + loss_dict["stft_loss"] * self.model.config.w_stft_loss
             )
+            if self.model.config.get("add_chroma", False):
+                loss_dict["loss"] = (
+                    loss_dict["loss"]
+                    + loss_dict["chroma_stft_loss"]
+                    * self.model.config.w_chroma_stft_loss
+                )
             if self.model.config.add_vq:
                 vq_states = output_dict["vq_states"]
                 vq_ids = output_dict["vq_ids"]
@@ -1025,6 +1023,7 @@ class Stage2(BaseStage):
                 loss_dict["vq_nuc"] = vq_nuc
                 loss_dict["vq_loss"] = vq_loss.sum()
                 loss_dict["vq_quant_rate"] = vq_quant_rate
+                loss_dict["vq_entropy"] = self.model.vq.embedding.entropy()
                 loss_dict["loss"] = (
                     loss_dict["loss"] + vq_loss.sum() * self.model.config.w_vq_loss
                 )
@@ -1466,6 +1465,14 @@ class Stage3(Stage2):
         vq_ids = shared_encoder_output["vq_ids"]
         return vq_ids
 
+    def training_step(self, batch, batch_idx):
+        loss_dict = self._shared_step(batch)
+        self.log_dict(loss_dict, prog_bar=True, sync_dist=True)
+        return loss_dict["loss"]
+
+    def validation_step(self, batch, batch_idx, dataloader_idx=0):
+        return
+
 
 class MKII(pl.LightningModule):
     def __init__(
@@ -1628,6 +1635,10 @@ class MKII(pl.LightningModule):
             loss_dict["nuc_syllable"] = nuc_syllable
             loss_dict["qr_chroma"] = qr_chroma
             loss_dict["qr_syllable"] = qr_syllable
+            loss_dict[
+                "entropy_vq_syllable"
+            ] = self.model.syllable_vq.embedding.entropy()
+            loss_dict["entropy_vq_chroma"] = self.model.chroma_vq.embedding.entropy()
         return loss_dict
 
     def training_step(self, batch, batch_idx):
@@ -1943,138 +1954,3 @@ class MKIIVocoder(pl.LightningModule):
         text_ids = encoded_text["input_ids"].to(batch["audio"].device)
         batch.update(text_ids=text_ids)
         return batch
-
-
-class SoundStorm(pl.LightningModule):
-    def __init__(
-        self,
-        model_cls,
-        criterion_cls,
-        optimizer_cls,
-        scheduler_cls,
-        required_modules=None,
-        checkpointing=False,
-        extra_params=None,
-    ):
-        super().__init__()
-        self.save_hyperparameters()
-        self.model = model_cls()
-        self.criterion = criterion_cls()
-        self.extra_params = DotDict(extra_params)
-        self.requires = {}
-        self.val_outputs = dict()
-
-        if checkpointing:
-            self.model.gradient_checkpointing_enable()
-
-    def setup(self, stage: str) -> None:
-        if (
-            stage == "fit"
-            and not self.requires
-            and self.hparams.required_modules is not None
-        ):
-            self.load_required_modules()
-
-    def load_required_modules(self):
-        for name, item in self.hparams.required_modules.items():
-            hpath = item["ckpt_path"]
-            init_fn = item["init_fn"]
-            cache_dir = item["cache_dir"]
-            print(f"Loading {name} from {hpath}")
-            self.requires.update(
-                init_fn(hpath, local_rank=self.local_rank, cache_dir=cache_dir)
-            )
-        return
-
-    @torch.no_grad()
-    @torch.cuda.amp.autocast(enabled=False)
-    def get_soundstream_tokens(self, x):
-        output = self.requires["ss"](x.float())[2]
-        output = torch.stack(output, dim=2)
-        return output
-
-    @torch.no_grad()
-    @torch.cuda.amp.autocast(enabled=False)
-    def get_mkii_tokens(self, x):
-        embeds, tokens = self.requires["mkii"].tokenize(x.float())
-        return tokens
-
-    @torch.no_grad()
-    @torch.cuda.amp.autocast(enabled=False)
-    def pad_audio(self, x):
-        return self.model.pad_audio(x)
-
-    @torch.no_grad()
-    @torch.cuda.amp.autocast(enabled=False)
-    def prepare_feature(self, batch):
-        wav = self.pad_audio(batch["audio"].squeeze(1)).float()
-        device = wav.device
-        b, _ = wav.size()
-
-        soundstream_ids = self.get_soundstream_tokens(wav)
-        mkii_ids = self.get_mkii_tokens(wav)
-        mkii_ids[:, :, 1] = (
-            mkii_ids[:, :, 1] + self.model.config.vq_syllable_codebook_size
-        )
-        mkii_ids = torch.reshape(mkii_ids, [b, -1]).unsqueeze(2)
-        return {"acoustic_tokens": soundstream_ids, "semantic_tokens": mkii_ids}
-
-    def _shared_step(self, batch):
-        input_dict = self.prepare_feature(batch)
-        acoustic_tokens = input_dict["acoustic_tokens"]
-        semantic_tokens = input_dict["semantic_tokens"]
-        output_dict = self.model(
-            acoustic_tokens=acoustic_tokens, semantic_tokens=semantic_tokens
-        )
-        logits = output_dict["logits"]
-        masked_acoustic_tokens = output_dict["masked_acoustic_tokens"]
-        rand_qs = output_dict["rand_qs"]
-
-        loss_dict = self.criterion(
-            logits=logits,
-            targets=acoustic_tokens,
-            masked_acoustic_tokens=masked_acoustic_tokens,
-            rand_qs=rand_qs,
-            mask_id=self.model.masking_scheme.mask_id,
-        )
-        return loss_dict
-
-    def training_step(self, batch, batch_idx):
-        loss_dict = self._shared_step(batch)
-        self.log_dict(loss_dict, prog_bar=True, sync_dist=True)
-        return loss_dict["loss"]
-
-    def validation_step(self, batch, batch_idx, dataloader_idx=0):
-        # Dummy function for triggering callbacks
-        return
-
-    def configure_optimizers(self):
-        optimizer = self.hparams.optimizer_cls(self.model.parameters())
-        scheduler = self.hparams.scheduler_cls(optimizer)
-        return {
-            "optimizer": optimizer,
-            "lr_scheduler": {"scheduler": scheduler, "interval": "step"},
-        }
-
-    @torch.no_grad()
-    @torch.cuda.amp.autocast(enabled=False)
-    def sample(
-        self,
-        semantic_tokens: torch.Tensor,
-        num_iterations: List[int],
-        score_strategies: List[str],
-        temperatures: Optional[List[float]] = None,
-    ) -> torch.Tensor:
-        return self.model.sample(
-            semantic_tokens=semantic_tokens,
-            num_iterations=num_iterations,
-            score_strategies=score_strategies,
-            temperatures=temperatures,
-        )
-
-    @torch.no_grad()
-    @torch.cuda.amp.autocast(enabled=False)
-    def token2audio(self, acoustic_tokens):
-        acoustic_tokens = acoustic_tokens.transpose(1, 2)  # [b, t, q] -> [b, q, t]
-        audio = self.requires["ss_dec"](acoustic_tokens).squeeze(1)
-        return audio

samantha/models/flash_llama.py
@@ -126,15 +126,18 @@ class LlamaRMSNorm(nn.Module):
 class LlamaRotaryEmbedding(torch.nn.Module):
     def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None):
         super().__init__()
-        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float().to(device) / dim))
-        self.register_buffer("inv_freq", inv_freq)
+        self.inv_freq = 1.0 / (
+            base ** (torch.arange(0, dim, 2).float().to(device) / dim)
+        )
 
         # Build here to make `torch.jit.trace` work.
-        self.max_seq_len_cached = max_position_embeddings
+        self._set_cos_sin_cached(max_position_embeddings)
+
+    @torch.cuda.amp.autocast(enabled=False)
+    def _set_cos_sin_cached(self, seq_len):
+        self.max_seq_len_cached = seq_len
         t = torch.arange(
-            self.max_seq_len_cached,
-            device=self.inv_freq.device,
-            dtype=self.inv_freq.dtype,
+            self.max_seq_len_cached, device=self.inv_freq.device, dtype=torch.float32
         )
         freqs = torch.einsum("i,j->ij", t, self.inv_freq)
         # Different from paper, but it uses a different permutation in order to obtain the same calculation
@@ -150,19 +153,8 @@ class LlamaRotaryEmbedding(torch.nn.Module):
         # x: [bs, num_attention_heads, seq_len, head_size]
         # This `if` block is unlikely to be run after we build sin/cos in `__init__`. Keep the logic here just in case.
         if seq_len > self.max_seq_len_cached:
-            self.max_seq_len_cached = seq_len
-            t = torch.arange(
-                self.max_seq_len_cached, device=x.device, dtype=self.inv_freq.dtype
-            )
-            freqs = torch.einsum("i,j->ij", t, self.inv_freq)
-            # Different from paper, but it uses a different permutation in order to obtain the same calculation
-            emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
-            self.register_buffer(
-                "cos_cached", emb.cos()[None, None, :, :], persistent=False
-            )
-            self.register_buffer(
-                "sin_cached", emb.sin()[None, None, :, :], persistent=False
-            )
+            self._set_cos_sin_cached(seq_len)
+
         return (
             self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
             self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype),