1ecf773db20c10fbc5cff3e6464448a3b40bf5b0
lafeihu <lafeihu@bytedance.com>
2024-01-03 11:32:43 +0800

    fix: LiteDataModule

--- samantha/dataio/lite/lite_datamodule.py
+++ samantha/dataio/lite/lite_datamodule.py
@@ -3,14 +3,22 @@ from typing import Any
 
 logger = logging.getLogger(__name__)
 
+try:
+    from pytorch_lightning import LightningDataModule
 
-class LiteDataModule:
+    _DM_BASE_CLS = LightningDataModule
+except ImportError:
+    _DM_BASE_CLS = object
+
+
+class LiteDataModule(_DM_BASE_CLS):
     def __init__(
         self,
         train_dataloader: Any = None,
         val_dataloader: Any = None,
         test_dataloader: Any = None,
     ):
+        super().__init__()
         self._train_dataloader = train_dataloader
         self._val_dataloader = val_dataloader
         self._test_dataloader = test_dataloader
