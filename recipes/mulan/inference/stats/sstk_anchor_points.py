57ce590aade70209b7681fafc7c34ecc77368e2a
andrew.shaw <andrew.shaw@bytedance.com>
2024-08-14 02:18:44 +0800

    v9.5 config and binary_center

--- recipes/mulan/inference/stats/sstk_anchor_points.py
+++ recipes/mulan/inference/stats/sstk_anchor_points.py
@@ -5,6 +5,8 @@ def load_anchor_points_from_mulan_ckpt(mulan_hpath):
         npy_path = 'recipes/mulan/inference/stats/binary_center_mulan_72.npy'
     elif 'mulan-step=005500-median_rank_1=63-kaggle' in mulan_hpath:
         npy_path = 'recipes/mulan/inference/stats/binary_center_mulan_63.npy'
+    elif 'mulan-step=005000-median_rank_1=61-kaggle' in mulan_hpath:
+        npy_path = 'recipes/mulan/inference/stats/binary_center_mulan_61.npy'
     else:
         print(
             'WARNING: load_anchor_points_from_mulan_ckpt could not find associated anchor points.',
