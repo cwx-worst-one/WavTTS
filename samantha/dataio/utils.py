0fca7e602256fe9a7b8674cfac738d582599d786
汪鑫 <wangxin.colin@bytedance.com>
2024-09-18 12:20:22 +0000

    feat: support global dataset id [skipped]
    * feat: support global dataset id
    * fix: lint
    * fix: lint
    
    https://code.byted.org/seed/samantha/merge_requests/1229

--- samantha/dataio/utils.py
+++ samantha/dataio/utils.py
@@ -24,6 +24,15 @@ except Exception:
     )
     get_dataset_collection_info_v2 = None
 
+try:
+    from bytedance.easycycle import get_dataset_collection_info_v3_with_idc
+except Exception:
+    warnings.warn(
+        "Could not import get_dataset_collection_info_v3_with_idc from bytedance.easycycle,"
+        "please upgrade your package."
+    )
+    get_dataset_collection_info_v3_with_idc = None
+
 from lightning_fabric.utilities.cloud_io import get_filesystem
 from lightning_fabric.utilities.exceptions import MisconfigurationException
 from pyarrow.parquet import ParquetFile
@@ -288,11 +297,16 @@ def resolve_data_urls(data_id=None, data_urls=None):
 
     if data_id is not None:
         os.environ["DatasetID"] = str(data_id)
-        if get_dataset_collection_info_v2 is not None:
-            try:
-                data_urls = get_dataset_collection_info_v2(data_id)["origin"]["paths"]
-            except Exception as e:
-                logger.error(e)
+
+        if get_dataset_collection_info_v3_with_idc is not None:
+            data_id = str(data_id)
+            if ":" in data_id:
+                logger.info(f"treat dataset id as idc format, {data_id=}")
+            data_urls = get_dataset_collection_info_v3_with_idc(data_id)["origin"][
+                "paths"
+            ]
+        elif get_dataset_collection_info_v2 is not None:
+            data_urls = get_dataset_collection_info_v2(data_id)["origin"]["paths"]
         else:
             data_urls = get_dataset_collection_info(data_id)
 
