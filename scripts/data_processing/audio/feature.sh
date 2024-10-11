e287797c0d3b568301da885657f7e6a64366e075
汪鑫 <wangxin.colin@bytedance.com>
2024-10-11 01:56:26 +0000

    fix: only fetch commit we need

--- scripts/data_processing/audio/feature.sh
+++ scripts/data_processing/audio/feature.sh
@@ -14,7 +14,8 @@ export BYTED_TORCH_C10D_LOG_LEVEL=ERROR
 COMMIT=${SAMANTHA_COMMIT:-$(git rev-parse --short HEAD)}
 
 branch_name=$(git branch --show-current)
-git fetch -q --all || echo "fetch failed"
+
+git fetch origin $COMMIT || echo "fetch $COMMIT failed"
 git fetch --unshallow || echo "on a complete repository"
 
 git checkout $COMMIT
