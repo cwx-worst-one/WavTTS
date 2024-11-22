31031c086b8e53850ef43bedbfb6bf479550030a
庄晓滨 <zhuangxiaobin@bytedance.com>
2024-11-22 04:05:32 +0000

    Zxb/v2a deploy [skipped]
    * video2audio v1.0
    * add v2a version2 (0.7b+0.7b)
    * add v2a(version2, 0.7b+0.7b, 16hz vae)
    * update v2.1
    * solve conflict
    * add v2a v2.2
    * add v2a v2.2
    * add v2a v2.2
    * [fixbug] video encoder inference
    * [fixbug] video encoder inference
    * [fixbug] video encoder inference
    * [v2a v3.0]update readme
    * fixbug
    * [v2a v3.0] video encoder inference
    
    https://code.byted.org/seed/samantha/merge_requests/1340

--- apps/bigtts/audiogen/README.md
+++ apps/bigtts/audiogen/README.md
@@ -13,24 +13,26 @@ CODE:
 ```
 mkdir .deploy_cache
 hdfs dfs -get hdfs://haruna/home/byte_data_seed/lf_lq/speech/checkpoints/user/zhuangxiaobin/v2a_deploy/models--apple--DFN2B-CLIP-ViT-B-16 .deploy_cache
-<<<<<<< HEAD
-
+```
 
 v3:
+
 ```
 hdfs dfs -get hdfs://haruna/home/byte_data_seed/lf_lq/speech/checkpoints/user/zhuangxiaobin/v2a_deploy/v2a_0.7b_0.3_v2_20k_encoder.ckpt .deploy_cache
 hdfs dfs -get hdfs://haruna/home/byte_data_seed/lf_lq/speech/checkpoints/user/zhuangxiaobin/v2a_deploy/v2a_0.7b_0.3_v2_20k_diffusion.ckpt .deploy_cache
 hdfs dfs -get hdfs://haruna/home/byte_data_seed/lf_lq/speech/checkpoints/user/zhuangxiaobin/v2a_deploy/v2a_0.7b_0.3_v2_20k_vocoder.ckpt .deploy_cache
 ```
-=======
->>>>>>> 73636a3c66d820f70b083bed41227b825d35b7e1
+
+inference:
+
+```
 python3 apps/bigtts/audiogen/soundify/v2a_infer.py 
 ```
 
 ```
-frames = v2a_model.read_video(in_video, target_fps=8, remove_caption=True) # remove_caption设置为 true，表示去除画面底部的 20%，对应字幕，最终还是用ocr来识别更合理
+frames = v2_reader.load(in_video) # 包含了去黑边的逻辑
 frames = frames.unsqueeze(0).to(device)
-wave = v2a_model.inference(frames) # 默认 cfg = 4.5， steps = 25
+wave = v2a_model.inference(frames) # 默认 cfg = 4.5， steps = 50
 save_audio(wave, out_audio)
 save_video(in_video, out_audio, out_video)
 ```
--- apps/bigtts/audiogen/soundify/v2a_infer.py
+++ apps/bigtts/audiogen/soundify/v2a_infer.py
@@ -229,11 +229,7 @@ class Soundify_v2a(nn.Module):
     def inference(self, frames, cfg_scale=7.5, step_num=50):
 
         with torch.autocast(device_type="cuda", dtype=torch.bfloat16, enabled=True):
-<<<<<<< HEAD
             video_embs = self.video_encoder.infer(frames)
-=======
-            video_embs = self.video_encoder(frames)
->>>>>>> 73636a3c66d820f70b083bed41227b825d35b7e1
             latents = self.diffusion.ddim_sample(video_embs=video_embs,
                                                  step_num=step_num,
                                                  cfg_scale=cfg_scale)
@@ -248,18 +244,14 @@ class Soundify_v2a(nn.Module):
 
 if __name__ == "__main__":
 
-<<<<<<< HEAD
-    in_video = ".deploy_cache/case0.mp4"
-=======
     in_video = ".deploy_cache/case3.mp4"
->>>>>>> 73636a3c66d820f70b083bed41227b825d35b7e1
 
     cavp_ckpt = ".deploy_cache/v2a_0.7b_0.3_v2_20k_encoder.ckpt"
     dit_ckpt = ".deploy_cache/v2a_0.7b_0.3_v2_20k_diffusion.ckpt"
     vocoder_ckpt = ".deploy_cache/v2a_0.7b_0.3_v2_20k_vocoder.ckpt"
 
     cfg_scale = 4.5
-    step_num = 30
+    step_num = 50
 
     device = "cuda"
 
@@ -275,13 +267,8 @@ if __name__ == "__main__":
     frames = v2_reader.load(in_video)
     frames = frames.unsqueeze(0).to(device)
 
-<<<<<<< HEAD
-    out_audio = "output/case0.wav"
-    out_video = "output/case0.mp4"
-=======
-    out_audio = "output/case3.wav"
-    out_video = "output/case3.mp4"
->>>>>>> 73636a3c66d820f70b083bed41227b825d35b7e1
+    out_audio = ".deploy_cache/case3.wav"
+    out_video = ".deploy_cache/case3.mp4"
     # inference
     wave = v2a_model.inference(frames, cfg_scale=cfg_scale, step_num=step_num)
     save_audio(wave, out_audio)
